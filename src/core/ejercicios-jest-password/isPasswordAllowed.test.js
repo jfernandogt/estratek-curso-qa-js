293bdc65797518408fd8d53ad20346aaa9b81c5b
Fernando Barrios <fer.b.1990@gmail.com>
2021-06-08 00:26:33 -0600

    refactor(core): Usar un generador de casos

--- src/core/ejercicios-jest-password/isPasswordAllowed.test.js
+++ src/core/ejercicios-jest-password/isPasswordAllowed.test.js
@@ -1,16 +1,21 @@
 import isPasswordAllowed from './isPasswordAllowed'
 import cases from 'jest-in-case'
 
+function generadorCasos(objCases) {
+  return Object.entries(objCases).map(([nombre, password]) => ({
+    name: `${nombre} - ${password}`,
+    password,
+  }))
+}
+
 cases(
   'isPasswordAllowed: contraseñas válidas',
   ({ password }) => {
     expect(isPasswordAllowed(password)).toBe(true)
   },
-  {
-    'contraseña válida': {
-      password: '!aBc123',
-    },
-  }
+  generadorCasos({
+    'contraseña válida': '!aBc123',
+  })
 )
 
 cases(
@@ -18,75 +23,12 @@ cases(
   ({ password }) => {
     expect(isPasswordAllowed(password)).toBe(false)
   },
-  {
-    'muy pequeña': {
-      password: 'a2c!',
-    },
-    'sin caracteres alfabeticos': {
-      password: '123456!',
-    },
-    'sin números': {
-      password: 'ABCdef!',
-    },
-    'sin mayúsculas': {
-      password: 'abc123!',
-    },
-    'sin minúsculas': {
-      password: 'ABC123!',
-    },
-    'sin números alphanuméricos': {
-      password: 'ABCdef123',
-    },
-  }
+  generadorCasos({
+    'muy pequeña': 'a2c!',
+    'sin caracteres alfabeticos': '123456!',
+    'sin números': 'ABCdef!',
+    'sin mayúsculas': 'abc123!',
+    'sin minúsculas': 'ABC123!',
+    'sin números alphanuméricos': 'ABCdef123',
+  })
 )
-
-// válido:
-// - !aBc123
-//
-// no válido:
-// - a2c! // muy pequeña
-// - 123456! // sin caracteres alfabeticos
-// - ABCdef! // sin números
-// - abc123! // sin mayúsculas
-// - ABC123! // sin minúsculas
-// - ABCdef123 // sin números alphanuméricos
-
-// describe('Probar isPasswordAllowed varios expect', () => {
-//   test('isPasswordAllowed debería retornar true cuando la contraseña es válida', () => {
-//     expect(isPasswordAllowed('!aBc123')).toBe(true)
-//   })
-
-//   test('isPasswordAllowed debería retornar false cuando la contraseña es inválida', () => {
-//     expect(isPasswordAllowed('a2c!')).toBe(false)
-
-//     expect(isPasswordAllowed('123456!')).toBe(false) // sin caracteres alfabeticos
-//     expect(isPasswordAllowed('ABCdef!')).toBe(false) // sin númeris
-//     expect(isPasswordAllowed('abc123!')).toBe(false) // sin mayúsculas
-//     expect(isPasswordAllowed('ABC123!')).toBe(false) // sin minúsculas
-//     expect(isPasswordAllowed('ABCdef123')).toBe(false) // sin números alphanuméricos
-//   })
-// })
-
-// describe('Probar isPasswordAllowed con forEach', () => {
-//   const validPasswords = ['!aBc123']
-
-//   const invalidPasswords = [
-//     '123456!',
-//     'ABCdef!',
-//     'abc123!',
-//     'ABC123!',
-//     'ABCdef123',
-//   ]
-
-//   validPasswords.forEach((password) => {
-//     test(`Acepta ${password}`, () => {
-//       expect(isPasswordAllowed(password)).toBe(true)
-//     })
-//   })
-
-//   invalidPasswords.forEach((password) => {
-//     test(`Rechaza ${password}`, () => {
-//       expect(isPasswordAllowed(password)).toBe(false)
-//     })
-//   })
-// })
