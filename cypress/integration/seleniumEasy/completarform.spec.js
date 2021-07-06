e781f7b5dafad84df5fdf3c51abfd5f1af30169e
Fernando Barrios <fer.b.1990@gmail.com>
2021-07-05 21:36:31 -0600

    refactor(core): Reemplazar fixtures por test-data-bot

--- cypress/integration/seleniumEasy/completarform.spec.js
+++ cypress/integration/seleniumEasy/completarform.spec.js
@@ -1,15 +1,26 @@
 /// <reference types="cypress" />
 
+import { build, fake, oneOf } from '@jackfranklin/test-data-bot'
+
+const usuarioConstructor = build({
+  fields: {
+    nombre: fake((f) => f.name.firstName()),
+    apellido: fake((f) => f.name.lastName()),
+    correo: fake((f) => f.internet.email()),
+    numero: fake((f) => f.phone.phoneNumber('##########')),
+    direccion: fake((f) => f.address.streetAddress()),
+    city: fake((f) => f.address.city()),
+    estado: fake((f) => f.address.state()),
+    zip: fake((f) => f.address.zipCode()),
+    sitioweb: fake((f) => f.internet.domainName()),
+    tieneHosting: oneOf('yes', 'no'),
+    comentario: fake((f) => f.lorem.text()),
+  },
+})
+
 context('Selenium Easy: Completar Formulario', () => {
   Cypress.on('uncaught:exception', () => {
     return false
-  })
-
-  before(() => {
-    cy.fixture('usuario').then((usuario) => {
-      // "this" is still the test context object
-      this.usuario = usuario
-    })
   })
 
   beforeEach(() => {
@@ -17,17 +28,18 @@ context('Selenium Easy: Completar Formulario', () => {
   })
 
   it('completar formulario', () => {
-    cy.get("input[name='first_name']").type(this.usuario.nombre)
-    cy.get("input[name='last_name']").type(this.usuario.apellido)
-    cy.get("input[name='email']").type(this.usuario.correo)
-    cy.get("input[name='phone']").type(this.usuario.numero)
-    cy.get("input[name='address']").type(this.usuario.direccion)
-    cy.get("input[name='city']").type(this.usuario.city)
-    cy.get("select[name='state']").select(this.usuario.estado)
-    cy.get("input[name='zip']").type(this.usuario.zip)
-    cy.get("input[name='website']").type(this.usuario.sitioweb)
-    cy.get(`input[type="radio"][value="${this.usuario.tieneHosting}"]`).check()
-    cy.get("textarea[name='comment']").type(this.usuario.comentario)
+    const usuario = usuarioConstructor()
+    cy.get("input[name='first_name']").type(usuario.nombre)
+    cy.get("input[name='last_name']").type(usuario.apellido)
+    cy.get("input[name='email']").type(usuario.correo)
+    cy.get("input[name='phone']").type(usuario.numero)
+    cy.get("input[name='address']").type(usuario.direccion)
+    cy.get("input[name='city']").type(usuario.city)
+    cy.get("select[name='state']").select(usuario.estado)
+    cy.get("input[name='zip']").type(usuario.zip)
+    cy.get("input[name='website']").type(usuario.sitioweb)
+    cy.get(`input[type="radio"][value="${usuario.tieneHosting}"]`).check()
+    cy.get("textarea[name='comment']").type(usuario.comentario)
     cy.get("button[type='submit']").click()
   })
 })
